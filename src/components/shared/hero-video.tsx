97123dcbf31f07a7f63f95dadb8895c9850dbf9d
Fedotos228 <tudor.onceanu@gmail.com>
2025-12-18 16:00:41 +0200

    changed hero video height on 2xl

--- src/components/shared/hero-video.tsx
+++ src/components/shared/hero-video.tsx
@@ -2,7 +2,7 @@ import { WPImages } from '@/types/wp.types'
 
 export default function HeroVideo({ video }: { video: WPImages }) {
   return (
-    <div className='lg:h-[600px] h-[500px] w-full relative overflow-hidden '>
+    <div className='2xl:h-[800px] lg:h-[600px] h-[500px] w-full relative overflow-hidden '>
       <video src={video.source_url} loop autoPlay muted className='w-full h-full object-cover' />
     </div>
   )
