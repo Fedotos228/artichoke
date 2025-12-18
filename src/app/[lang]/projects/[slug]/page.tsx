ecc4ce8a3b20d3a0a5b4261ed142de52354652f2
Fedotos228 <tudor.onceanu@gmail.com>
2025-12-18 12:31:28 +0200

    am inteles preview ebanai

--- src/app/[lang]/projects/[slug]/page.tsx
+++ src/app/[lang]/projects/[slug]/page.tsx
@@ -3,10 +3,19 @@ import ProjectGallery from '@/components/pages/project-single/project-gallery'
 import ProjectHero from '@/components/pages/project-single/project-hero'
 import { Locale } from '@/i18n-config'
 import { getSingleProject, getSingleProjectMetadata } from '@/services/projects.service'
+import { ProjectSlugTypes } from '@/types/projects.type'
 import { Metadata } from 'next'
 import { cookies } from 'next/headers'
 import Script from 'next/script'
 
+
+export async function generateStaticParams() {
+  const projects = await fetch(`${process.env.WP_URL}/projects?_fields=slug&lang=en`).then(res => res.json()) as ProjectSlugTypes
+
+  return projects.map((post) => ({
+    slug: post.slug
+  }))
+}
 
 export async function generateMetadata(
   { params }: { params: { slug: string } }
